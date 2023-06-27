f52b3006d5c47def769969b5ebde1c8f2ce41d0d
Flavio Nascimento <54750750+FlavioNascimento99@users.noreply.github.com>
2023-06-27 13:23:50 -0300

    app module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,36 @@
+// Angular;
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from "./app-routing.module";
-import { CommonModule } from '@angular/common';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { ProfileComponent } from './user/user-profile/profile.component';
-import { LoginComponent } from './user/user-login/login.component';
-import { ErrorComponent } from './components/shared/error/error.component';
-import { MatCardModule } from "@angular/material/card";
-import { CreateComponent } from './posts/post-create/create.component';
-import { MatInputModule} from "@angular/material/input";
-import { MatSelectModule} from "@angular/material/select";
+
+// 
+import { FormsModule } from "@angular/forms";
+import { PostsModule } from "./posts/posts.module";
 import { HomeComponent } from './components/frames/home/home.component';
 import { ListComponent } from './posts/post-list/list.component';
+import { ErrorComponent } from './components/shared/error/error.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { CreateComponent } from './posts/post-create/create.component';
+import { ProfileComponent } from './user/user-profile/profile.component';
+
+// (todo: remove all of those imports at next commit)
+import { LoginComponent } from './user/user-login/login.component';
 import { SigninComponent } from './user/user-sign-in/signin.component';
-import {FormsModule} from "@angular/forms";
-import {MatListModule} from "@angular/material/list";
-import {PostsModule} from "./posts/posts.module";
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import {MatMenuModule} from "@angular/material/menu";
+
+
+// Material Design Imports (todo: replace every those imports at once component)
+import { MatMenuModule } from "@angular/material/menu";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 
 @NgModule({
   declarations: [
